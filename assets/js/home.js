65bcd66db80d159b0e0c746f665b975d9d1c8e96
20SB <subhabiswal20@gmail.com>
2023-10-10 00:26:57 +0530

    home page error removed

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,65 +1,71 @@
-console.log("skills js loaded")
-const skillsArray = [
-    "Frontend Development",
-    "Backend Development"
-];
+{
+    let skillsArray = ["Frontend Development", "Backend Development"];
 
-const skillIndex = { index: 0 };
+    const skillIndex = { index: 0 };
 
-function typeAndErase(element, text, typingSpeed, erasingSpeed) {
-    return new Promise((resolve) => {
-        let index = 0;
-        let isErasing = false;
+    function typeAndErase(element, text, typingSpeed, erasingSpeed) {
+        return new Promise((resolve) => {
+            let index = 0;
+            let isErasing = false;
 
-        function type() {
-            if (isErasing) {
-                element.textContent = text.slice(0, index);
-                index--;
-            } else {
-                element.textContent = text.slice(0, index);
-                index++;
+            function type() {
+                if (isErasing) {
+                    element.textContent = text.slice(0, index);
+                    index--;
+                } else {
+                    element.textContent = text.slice(0, index);
+                    index++;
+                }
+
+                if (!isErasing && index <= text.length) {
+                    setTimeout(type, typingSpeed);
+                } else if (!isErasing) {
+                    setTimeout(() => {
+                        isErasing = true;
+                        setTimeout(type, erasingSpeed); // Delay before erasing
+                    }, 1000); // Delay before starting to erase
+                } else if (isErasing && index >= 0) {
+                    setTimeout(type, erasingSpeed);
+                } else {
+                    isErasing = false;
+                    element.textContent = ""; // Clear the element's text
+                    resolve(); // Resolve the promise to continue with the next skill
+                }
             }
 
-            if (!isErasing && index <= text.length) {
-                setTimeout(type, typingSpeed);
-            } else if (!isErasing) {
-                setTimeout(() => {
-                    isErasing = true;
-                    setTimeout(type, erasingSpeed); // Delay before erasing
-                }, 1000); // Delay before starting to erase
-            } else if (isErasing && index >= 0) {
-                setTimeout(type, erasingSpeed);
-            } else {
-                isErasing = false;
-                element.textContent = ''; // Clear the element's text
-                resolve(); // Resolve the promise to continue with the next skill
-            }
-        }
-
-        type();
-    });
-}
-
-async function typeNextSkill() {
-    if (skillIndex.index < skillsArray.length) {
-        const skill = skillsArray[skillIndex.index];
-        const typingSpeed = 100; // Typing speed in milliseconds
-        const erasingSpeed = 0.3 * typingSpeed; // Erasing speed is double the typing speed
-        const skillElement = document.querySelector('.skill');
-        skillElement.textContent = ''; // Clear the original text
-
-        // First, type the skill
-        await typeAndErase(skillElement, skill, typingSpeed, erasingSpeed);
-
-        skillIndex.index++;
-        if (skillIndex.index >= skillsArray.length) {
-            skillIndex.index = 0; // Reset the index to loop through skills
-        }
-        typeNextSkill(); // Type the next skill
+            type();
+        });
     }
+
+    async function typeNextSkill() {
+        if (skillIndex.index < skillsArray.length) {
+            const skill = skillsArray[skillIndex.index];
+            const typingSpeed = 100; // Typing speed in milliseconds
+            const erasingSpeed = 0.3 * typingSpeed; // Erasing speed is double the typing speed
+            const skillElement = document.querySelector(".skill");
+
+            if (skillElement) {
+                // Check if the element exists
+                skillElement.textContent = ""; // Clear the original text
+
+                // First, type the skill
+                await typeAndErase(
+                    skillElement,
+                    skill,
+                    typingSpeed,
+                    erasingSpeed
+                );
+
+                skillIndex.index++;
+                if (skillIndex.index >= skillsArray.length) {
+                    skillIndex.index = 0; // Reset the index to loop through skills
+                }
+                typeNextSkill(); // Type the next skill
+            } else {
+                
+            }
+        }
+    }
+
+    typeNextSkill(); // Start typing the skills sequentially
 }
-
-typeNextSkill(); // Start typing the skills sequentially
-
-
-
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,62 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var loadedSections = {};
-
+document.addEventListener("DOMContentLoaded", function () {
     // Function to load content via AJAX
+    var backButton = document.getElementById("back-button");
+    var currentSection = "home"; // Initialize current section to 'home'
+    var prevSection = null; // Initialize previous section to null
+
+    // Define the loadScript function to load JavaScript files dynamically
+    function loadScript(url) {
+        var script = document.createElement("script");
+        script.src = url;
+        document.head.appendChild(script);
+    }
+
     function loadContent(section) {
-        if (loadedSections[section]) {
-            // Section content is already loaded
-            return;
+        if (section == "home") {
+            backButton.classList.add("disp-none");
+        } else {
+            backButton.classList.remove("disp-none");
         }
+        prevSection = currentSection; // Store the current section as the previous section
+        currentSection = section; // Update the current section
 
         var xhr = new XMLHttpRequest();
-        var contentUrl = 'sections/' + section + '.html'; // Include the folder path
-        var scriptUrl = 'assets/js/' + section + '.js'; // Include the folder path
-        xhr.open('GET', contentUrl, true);
-        xhr.onreadystatechange = function() {
+        var contentUrl = "sections/" + section + ".html"; // Include the folder path
+        var scriptUrl = "assets/js/" + section + ".js"; // Include the folder path
+        xhr.open("GET", contentUrl, true);
+        xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
-                document.getElementById('main-content').innerHTML = xhr.responseText;
-                loadScript(scriptUrl);
-                loadedSections[section] = true;
+                document.getElementById("main-content").innerHTML =
+                    xhr.responseText;
+                loadScript(scriptUrl); // Call the loadScript function to load the JavaScript file
             }
         };
         xhr.send();
     }
 
-    function loadScript(url) {
-        var script = document.createElement('script');
-        script.src = url;
-        document.head.appendChild(script);
-    }
-
     // Handle clicks on navigation links
-    var navLinks = document.querySelectorAll('nav a');
-    navLinks.forEach(function(link) {
-        link.addEventListener('click', function(e) {
+    var navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach(function (link) {
+        link.addEventListener("click", function (e) {
             e.preventDefault();
-            var section = e.target.getAttribute('href').substring(1);
+            var section = e.target.getAttribute("href").substring(1);
+            
             loadContent(section);
         });
     });
 
     // Load initial content
-    loadContent('home');
+    loadContent("home");
+
+    // Handle back button click
+    if (backButton) {
+        backButton.addEventListener("click", function (e) {
+            e.preventDefault();
+            if (prevSection !== null) {
+                loadContent(prevSection); // Load the previous section when the back button is clicked
+            }
+        });
+    }
 
     let scrollPercentage = () => {
         let scrollProgress = document.getElementById("progress");
@@ -54,7 +71,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         scrollProgress.style.background = `conic-gradient(#525252 ${scrollValue}%, #b9b9b9 ${scrollValue}%)`;
         progressValue.textContent = `${scrollValue}%`;
-
     };
 
     // Use the "scroll" event on the #main-content div
